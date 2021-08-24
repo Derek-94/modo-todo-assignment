6c3ad2a3fb6b06183bb773e63328424313474f98
bohyunkang <bbohyun@kakao.com>
2021-08-24 17:48:11 +0900

    Style: GlobalStyles, ThemeProvider 적용. (#8)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from 'styles/GlobalStyles';
+import { theme } from 'styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <ThemeProvider theme={{ ...theme }}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
