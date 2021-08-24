8cfe4ebb5f0c05d6abd1a00d88bc5d2a7f10cc74
bohyunkang <bbohyun@kakao.com>
2021-08-25 00:49:44 +0900

    Style: Button, Label 재사용 컴포넌트 설계 및 스타일링. (#8)

---
+++ src/components/common/Label.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface priorityProps {
+  High?: boolean;
+  Medium?: boolean;
+  Low?: boolean;
+}
+
+const setSize = (props: priorityProps) => {
+  if (props.High) {
+    return css`
+      background-color: #ea2027;
+    `;
+  }
+
+  if (props.Medium) {
+    return css`
+      background-color: #ffc312;
+    `;
+  }
+
+  return css`
+    background-color: #009432;
+  `;
+};
+
+const Label: React.FC<priorityProps> = ({ children, ...props }) => {
+  return <LabelWrap {...props}>{children}</LabelWrap>;
+};
+
+const LabelWrap = styled.button<priorityProps>`
+  padding: ${({ theme }) => theme.layout.badgePadding};
+  border-radius: ${({ theme }) => theme.layout.radius};
+  ${props => setSize(props)}
+`;
+
+export default Label;
