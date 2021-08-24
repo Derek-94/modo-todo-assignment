c80cfed699e85d3986efe20aa0a306ba8a3971e0
ssong10 <leesy1403@naver.com>
2021-08-24 14:30:19 +0900

    Fix: Itodo type, Modify dummy data

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,13 +1,13 @@
 import { Itodo, StatusKey } from 'types';
 
 export const TODOS: Itodo[] = [
-  { id: 1, taskName: '기본세팅', status: 'Done' },
-  { id: 2, taskName: '원티드 프로젝트', status: 'InProgress' },
-  { id: 3, taskName: '자소서 쓰기', status: 'Todo' },
-  { id: 4, taskName: '리드미 작성', status: 'Todo' },
-  { id: 5, taskName: '거리 두기', status: 'InProgress' },
-  { id: 6, taskName: '회의', status: 'Todo' },
-  { id: 7, taskName: '캐치마인드', status: 'Todo' },
+  { id: 1, taskName: '기본세팅', status: 'Done', priority: 'medium' },
+  { id: 2, taskName: '원티드프로젝트', status: 'InProgress', priority: 'high' },
+  { id: 3, taskName: '자소서 쓰기', status: 'Todo', priority: 'medium' },
+  { id: 4, taskName: '리드미 작성', status: 'Todo', priority: 'medium' },
+  { id: 5, taskName: '거리 두기', status: 'InProgress', priority: 'medium' },
+  { id: 6, taskName: '회의', status: 'Todo', priority: 'medium' },
+  { id: 7, taskName: '캐치마인드', status: 'Todo', priority: 'medium' },
 ];
 
 export const STATUS: StatusKey[] = ['Todo', 'InProgress', 'Done'];
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,11 @@ export interface Itodo {
   id: number;
   taskName: string;
   status: StatusKey;
+  priority?: PriorityType;
+  createdAt?: string;
+  updatedAt?: string;
+  dueDate?: string;
 }
+
+export type PriorityType = 'low' | 'medium' | 'high';
 export type StatusKey = 'Todo' | 'InProgress' | 'Done';
