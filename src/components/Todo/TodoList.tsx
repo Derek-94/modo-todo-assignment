d30cfe8b0f170264b499fa30ddf2a7d7f8b4fb82
bohyunkang <bbohyun@kakao.com>
2021-08-26 16:44:20 +0900

    Style: 필터링 컴포넌트 스타일링 수정

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -63,7 +63,7 @@ const TodoFormWrapper = styled.div`
   padding-top: 80px;
   display: flex;
   justify-content: center;
-  align-item: center;
+  align-items: center;
 `;
 
 const MainContainer = styled.main`
